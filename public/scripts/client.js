8cfdcf5bdc1d4160c83e26b1a5923419c0715c02
matt <mattcllhn@gmail.com>
2016-09-27 14:47:30 -0500

    logIn function working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,6 @@
 console.log('js sourced');
 var myApp = angular.module('myApp',[]);
+var lock = new Auth0Lock('PLvMpMQ4IPOQ8Xl7AHpxYHTFXq9Nt1Or','mattcllhn.auth0.com');
 
 myApp.controller('authController', ['$scope','$http',function($scope,$http){
   console.log('in authController');
@@ -7,5 +8,16 @@ myApp.controller('authController', ['$scope','$http',function($scope,$http){
   $scope.showNHide = function(){
     $scope.showStuff = !$scope.showStuff;
 
-  };
+  };//showNHide
+
+$scope.logIn = function(){
+  console.log('button click works');
+lock.show(function(err,profile,token){
+  if(err){
+    console.log(err);
+  }else {
+    console.log('success!',profile);
+  }
+});//lock.show
+};//logIn
 }]);//myapp controller
