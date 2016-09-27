46b96bf7db94faef643de31309d720d431e585be
matt <mattcllhn@gmail.com>
2016-09-27 13:59:23 -0500

    ng show hide

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,11 @@
 console.log('js sourced');
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('authController', ['$scope','$http',function($scope,$http){
+  console.log('in authController');
+
+  $scope.showNHide = function(){
+    $scope.showStuff = !$scope.showStuff;
+
+  };
+}]);//myapp controller
